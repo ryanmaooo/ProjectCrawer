fd9e757edbe332d140ef16236b7b76f8142478a5
ryanmaooo <vvvv0919@gmail.com>
2017-04-24 19:22:57 +0800

    crawer1

--- crawer1.py
+++ crawer1.py
@@ -15,27 +15,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get("https://www.bnext.com.tw/articles")
+
+res = requests.get("https://www.bnext.com.tw/article/44178/amazon-global-selling-build-taiwan-service-team")
 res.encoding = 'utf'
-soup = BeautifulSoup(res.text,"lxml")                                   #指定html5lib解析器 lxml亦可 , lxml速度較 html5lib 快
+soup = BeautifulSoup(res.text, "lxml")                              #指定html5lib解析器 lxml亦可 , lxml速度較 html5lib 快
 
-# for soup2 in soup.select('.container-fluid .div_td .item_summary'):                       #用for迴圈取 會按照網頁<p>順序依序取出
-#     print(soup2.get_text(separator="\n\n",strip=True))                                      #只取內容 去除<p>
-i = 0
-links= []
-for soup2 in soup.select('.font_sty02'):
+soup2 =[]
+for soup2 in soup.select('#article_view_body > div.main_block > div > div > div.content.htmlview > div > div.left p, h2, #article_view_body > div.main_block > div > div > div.article_info.container-fluid > span'):                       #用for迴圈取 會按照網頁<p>順序依序取出
+    res = [soup2.get_text(separator="\n\n",strip=True)]        #只取內容 去除<p>
 
-    links.append(soup2.parent['href'])
-    res2 = requests.get('"'+links[i]+'"')
-    i = i+1
-    res2.encoding = 'utf'
-    soup2 = BeautifulSoup(res2.text,"lxml")
+    # print(type(soup2))
+# i = 0
+# links= []
+# for soup2 in soup.select('.font_sty02'):
+#     links.append(soup2.parent['href'])
+#     res2 = requests.get(links[i])
+#     i+=1
+#     res2.encoding = 'utf'
+#     soup2 = BeautifulSoup(res2.text,"lxml")
+#
+#     for soup3 in soup2.select('.left p , h2'):
+#        print(soup3.get_text(separator="\n\n", strip=True))
 
-    for soup3 in soup2.select('.container-fluid .div_td .item_summary'):                       #用for迴圈取 會按照網頁<p>順序依序取出
-        print(soup3.get_text(separator="\n\n",strip=True))
-#  print(len(links))
+
+# print(len(links))
 # print(links)
-
+# (separator="\n\n", strip=True)
 
 
 
